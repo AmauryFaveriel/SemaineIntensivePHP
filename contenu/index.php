09bd826ca1638f896af53ed6a988c12cd245faf0
Amaury Faveriel <mrfvrl@gmail.com>
2018-02-14 23:57:13 +0100

    modified php and add details page

--- contenu/index.php
+++ contenu/index.php
@@ -26,17 +26,22 @@ $stmt->execute();
     <div class="error"><?=$_GET['error']?></div>
 <?php } ?>
 
+<h1>Motor Legends</h1>
+
 <div class="container">
     <?php while (false !== $row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
 
         <div class="carContainer">
-            <img class="carImage" src="img/<?=$row['img']?>" alt="<?=$row['marque'].' '.$row['modele']?>">
-            <h2 class="firstCar-image-brand"><?=$row['marque'].' '.$row['modele']?></h2>
-            <h3><?=$row['annee']?></h3>
+            <a href="details.php?id=<?=$row['id']?>">
+                <img class="carImage" src="img/<?=$row['img']?>" alt="<?=$row['marque'].' '.$row['modele']?>">
+                <h2 class="firstCar-image-brand"><?=$row['marque'].' '.$row['modele']?></h2>
+                <h3><?=$row['annee']?></h3>
+            </a>
         </div>
 
     <?php endwhile;?>
 </div>
+<a href="admin.php">Admin</a>
 
 </body>
 </html>
