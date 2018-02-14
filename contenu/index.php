ed438a6c8cbcb5aab37a198a6c6179a54d13e621
Amaury Faveriel <mrfvrl@gmail.com>
2018-02-14 23:12:30 +0100

    modified index.php and style.css

--- contenu/index.php
+++ contenu/index.php
@@ -12,31 +12,31 @@ $requete = "SELECT
 $stmt = $conn->prepare($requete);
 $stmt->execute();
 ?>
-<!doctype html>
-<html lang="en">
+
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <title>Motor Legend</title>
 </head>
 <body>
-    <?php if (isset($_GET['error'])) { ?>
-        <div class="error"><?=$_GET['error']?></div>
-    <?php } ?>
+<?php if (isset($_GET['error'])) { ?>
+    <div class="error"><?=$_GET['error']?></div>
+<?php } ?>
 
-    <h1 class="title">Nos voitures</h1>
-
+<div class="container">
     <?php while (false !== $row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
 
-        <img src="img/<?=$row['img']?>" alt="<?=$row['marque'].' '.$row['modele']?>">
-        <h2 class="carName"><?=$row['marque'].' '.$row['modele']?></h2>
-        <h3><?=$row['annee']?></h3>
+        <div class="carContainer">
+            <img class="carImage" src="img/<?=$row['img']?>" alt="<?=$row['marque'].' '.$row['modele']?>">
+            <h2 class="firstCar-image-brand"><?=$row['marque'].' '.$row['modele']?></h2>
+            <h3><?=$row['annee']?></h3>
+        </div>
 
     <?php endwhile;?>
+</div>
 
-    <a href="admin.php">Panneau administrateur</a>
 </body>
 </html>
