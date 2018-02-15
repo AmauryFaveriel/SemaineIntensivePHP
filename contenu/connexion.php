518bfa7eeb53051189a6913606abc02c347ff7fa
SophiiaG <sophiagounani@gmail.com>
2018-02-15 15:24:22 +0100

    Comments
    Ajout de commentaires

--- contenu/connexion.php
+++ contenu/connexion.php
@@ -5,3 +5,7 @@ try {
     die($exception->getMessage());
 }
 ?>
+
+<!-- la variable connexion permet de lié le php à ma base de donnée pour intéragir avec elle et catch c'est l'exeption,
+ c'est à dire l'erreur. Si il y as une erreur on affiche un message -->
+
